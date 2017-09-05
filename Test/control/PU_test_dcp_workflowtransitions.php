75f34ff68314e7535ccf2d381b8f28b6adf67aed
Jérôme Augé <jerome.auge@anakeen.com>
2017-09-05 15:43:28 +0200

    feat(PU): Add test for transitions with array's columns declared needed=Y (refs #7061)
    Add tests in `TestWorkflowTransition` for transition with empty array's
    columns declared needed='Y' which should not throws errors.

--- Test/control/PU_test_dcp_workflowtransitions.php
+++ Test/control/PU_test_dcp_workflowtransitions.php
@@ -29,17 +29,21 @@ class TestWorflowTransition extends TestCaseDcpCommonFamily
      * @param $end
      * @param $passExpectedError
      */
-    public function testTransitionCondition($start, $end, $passExpectedError)
+    public function testTransitionCondition($famId, $start, $end, $passExpectedError, $customStore = null)
     {
         $wf = new_doc(self::$dbaccess, "WTST_M0M3");
         $this->assertTrue($wf->isAlive() , "cannot find document WTST_M0M3");
         
-        $d = createDoc(self::$dbaccess, "TST_WFFAMM0M3");
+        $d = createDoc(self::$dbaccess, $famId);
         $d->setTitle('zou');
         $d->wid = $wf->id;
         $d->state = $start;
-        $err = $d->store();
-        $this->assertEmpty($err, "cannot create TST_WFFAMM0M3 document");
+        if (is_callable($customStore)) {
+            $err = $customStore($d);
+        } else {
+            $err = $d->store();
+        }
+        $this->assertEmpty($err, "cannot create ${famId} document");
         
         $err = $d->setState($end);
         if ($passExpectedError == '') {
@@ -55,17 +59,17 @@ class TestWorflowTransition extends TestCaseDcpCommonFamily
      * @param $end
      * @param $passExpectedMsg
      */
-    public function testTransitionPostAction($start, $end, $passExpectedMsg)
+    public function testTransitionPostAction($famId, $start, $end, $passExpectedMsg)
     {
         $wf = new_doc(self::$dbaccess, "WTST_M0M3");
         $this->assertTrue($wf->isAlive() , "cannot find document WTST_M0M3");
         
-        $d = createDoc(self::$dbaccess, "TST_WFFAMM0M3");
+        $d = createDoc(self::$dbaccess, $famId);
         $d->setTitle('zou');
         $d->wid = $wf->id;
         $d->state = $start;
         $err = $d->store();
-        $this->assertEmpty($err, "cannot create TST_WFFAMM0M3 document");
+        $this->assertEmpty($err, "cannot create ${famId} document");
         
         $err = $d->setState($end, '', false, true, true, true, true, true, true, $msg);
         $this->assertEmpty($err, sprintf("transition %s -> %s is not passed : %s", $start, $end, $err));
@@ -80,29 +84,96 @@ class TestWorflowTransition extends TestCaseDcpCommonFamily
     {
         return array(
             array(
+                'TST_WFFAMM0M3',
                 \WTestM0M3::SA,
                 \WTestM0M3::SC,
                 ''
             ) ,
             array(
+                'TST_WFFAMM0M3',
                 \WTestM0M3::SA,
                 \WTestM0M3::SB,
                 'm0 forbidden'
             ) ,
             array(
+                'TST_WFFAMM0M3',
                 \WTestM0M3::SA,
                 \WTestM0M3::SD,
                 ''
             ) ,
             array(
+                'TST_WFFAMM0M3',
                 \WTestM0M3::SB,
                 \WTestM0M3::SC,
                 'm1 forbidden'
             ) ,
             array(
+                'TST_WFFAMM0M3',
                 \WTestM0M3::SB,
                 \WTestM0M3::SD,
                 ''
+            ) ,
+            array(
+                'TST_WFFAM_DEV_7061',
+                \WTestM0M3::SB,
+                \WTestM0M3::SD,
+                ''
+            ) ,
+            array(
+                'TST_WFFAM_DEV_7061',
+                \WTestM0M3::SB,
+                \WTestM0M3::SD,
+                'Texte colonne 1 doit être rempli',
+                function (\Dcp\Family\Document & $doc)
+                {
+                    $doc->setVAlue('TXT_COLTEXT_1', array(
+                        // Missing/empty TST_COLTEXT_1 altogether
+                        
+                    ));
+                    $doc->setValue('TST_COLTEXT_2', array(
+                        'Row 1',
+                        'Row 2'
+                    ));
+                    return $doc->store();
+                }
+            ) ,
+            array(
+                'TST_WFFAM_DEV_7061',
+                \WTestM0M3::SB,
+                \WTestM0M3::SD,
+                'Texte colonne 1 doit être rempli',
+                function (\Dcp\Family\Document & $doc)
+                {
+                    $doc->setValue('TXT_COLTEXT_1', array(
+                        '', // First cell is empty
+                        'Row 2'
+                    ));
+                    $doc->setValue('TST_COLTEXT_2', array(
+                        'Row 1',
+                        'Row 2'
+                    ));
+                    return $doc->store();
+                }
+            ) ,
+            array(
+                'TST_WFFAM_DEV_7061',
+                \WTestM0M3::SB,
+                \WTestM0M3::SD,
+                'Texte colonne 1 doit être rempli',
+                function (\Dcp\Family\Document & $doc)
+                {
+                    $doc->setValue('TXT_COLTEXT_1', array(
+                        'Row 1',
+                        ''
+                        // Last cell is empty
+                        
+                    ));
+                    $doc->setValue('TST_COLTEXT_2', array(
+                        'Row 1',
+                        'Row 2'
+                    ));
+                    return $doc->store();
+                }
             )
         );
     }
@@ -111,16 +182,19 @@ class TestWorflowTransition extends TestCaseDcpCommonFamily
     {
         return array(
             array(
+                'TST_WFFAMM0M3',
                 \WTestM0M3::SA,
                 \WTestM0M3::SC,
                 ''
             ) ,
             array(
+                'TST_WFFAMM0M3',
                 \WTestM0M3::SA,
                 \WTestM0M3::SD,
                 'm3 pass'
             ) ,
             array(
+                'TST_WFFAMM0M3',
                 \WTestM0M3::SB,
                 \WTestM0M3::SD,
                 'm2 pass'
@@ -128,4 +202,3 @@ class TestWorflowTransition extends TestCaseDcpCommonFamily
         );
     }
 }
-?>
